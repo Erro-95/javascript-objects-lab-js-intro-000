4a3f206d29af9c71f69ab274b67ab7c53bc48dbc
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-29 21:28:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
 }
 
 function deleteFromObjectByKey(obj, key){
-  return Object.assign({}, obj, delete newObj[key])
+  return Object.assign({}, obj, {delete [key]})
 }
 
 function destructivelyDeleteFromObjectByKey(obj, key){
