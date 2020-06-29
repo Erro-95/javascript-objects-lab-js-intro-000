7d9367813a94a627e7b657ccadf2719a5708f529
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-06-29 20:49:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ function updateObjectWithKeyAndValue(obj, key, value){
 }
 
 
-destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   return obj[key] = value;
 }
 
-deleteFromObjectByKey(obj, key){
+function deleteFromObjectByKey(obj, key){
   return object.assign({}, obj, delete key);
 }
 
-destructivelyDeleteFromObjectByKey(obj, key){
+function destructivelyDeleteFromObjectByKey(obj, key){
   return delete obj[key];
 }
